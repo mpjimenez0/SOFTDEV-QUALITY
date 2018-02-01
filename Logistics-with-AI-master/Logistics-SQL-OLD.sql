ffff190fd2f98a274c86efa296811e544cff4c00
mpjimenez0 <cramjimenez07@gmail.com>
2018-02-01 13:25:01 +0800

    fuckthis
    screwed up reportico

--- Logistics-with-AI-master/Logistics-SQL-OLD.sql
+++ Logistics-with-AI-master/Logistics-SQL-OLD.sql
@@ -454,6 +454,7 @@ CREATE TABLE `request` (
   `vehicle_plate_number` varchar(20) DEFAULT NULL,
   `supply_code` int(11) DEFAULT NULL,
   `volunteer` int(11) DEFAULT NULL,
+
   `volunteer_occupation` varchar(255) DEFAULT NULL,
   `vehicle_type` varchar(45) DEFAULT NULL,
   `vehicle_name` varchar(255) DEFAULT NULL,
